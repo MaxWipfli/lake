2c5185543d7cce6941426904dd744b5f3343ee95
Max Wipfli <mail@maxwipfli.ch>
2022-12-27 20:44:30 +0100

    Add incomplete type_traits.hpp
    This adds a type_traits.hpp header, which currently only contains a
    remove_reference struct, and a remove_reference_t helper type.

---
+++ include/lake/type_traits.hpp
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Max Wipfli <mail@maxwipfli.ch>
+ * SPDX-LicenseIdentifier: MIT
+ */
+
+#pragma once
+
+namespace lake {
+
+template <typename T>
+struct remove_reference {
+    using type = T;
+};
+template <typename T>
+struct remove_reference<T&> {
+    using type = T;
+};
+template <typename T>
+struct remove_reference<T&&> {
+    using type = T;
+};
+
+template <typename T>
+using remove_reference_t = typename remove_reference<T>::type;
+
+}
