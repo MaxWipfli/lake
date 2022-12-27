fb11dea59fdc34771da5da3632fdfbeebbbd28ac
Max Wipfli <mail@maxwipfli.ch>
2022-12-27 20:51:42 +0100

    Implement forward() in extras.hpp
    This should behave exactly like std::forward.

--- include/lake/extras.hpp
+++ include/lake/extras.hpp
@@ -10,6 +10,17 @@
 namespace lake {
 
 template <typename T>
+constexpr T&& forward(remove_reference_t<T>& value) noexcept
+{
+    return static_cast<T&&>(value);
+}
+template <typename T>
+constexpr T&& forward(remove_reference_t<T>&& value) noexcept
+{
+    return static_cast<T&&>(value);
+}
+
+template <typename T>
 constexpr remove_reference_t<T>&& move(T&& value) noexcept
 {
     return static_cast<remove_reference_t<T>&&>(value);
