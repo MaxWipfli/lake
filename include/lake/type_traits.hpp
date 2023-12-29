a2796871114678fb1c784fced2a53657e85930d8
Max Wipfli <mail@maxwipfli.ch>
2023-12-29 13:43:46 +0100

    extras: Add lake::remove_const<T> helper

--- include/lake/type_traits.hpp
+++ include/lake/type_traits.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 Max Wipfli <mail@maxwipfli.ch>
+ * SPDX-FileCopyrightText: 2022-2023 Max Wipfli <mail@maxwipfli.ch>
  * SPDX-LicenseIdentifier: MIT
  */
 
@@ -7,6 +7,20 @@
 
 namespace lake {
 
+// remove_const
+template <typename T>
+struct remove_const {
+    using type = T;
+};
+template <typename T>
+struct remove_const<T const> {
+    using type = T;
+};
+
+template <typename T>
+using remove_const_t = typename remove_const<T>::type;
+
+// remove_reference
 template <typename T>
 struct remove_reference {
     using type = T;
